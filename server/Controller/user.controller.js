cc07927e9ed541f5ccadcebcf87b6a51a15821c6
Aryaan007-sudo <blankgaming75@gmail.com>
2025-03-25 10:33:08 +0530

    jwt

--- server/Controller/user.controller.js
+++ server/Controller/user.controller.js
@@ -1,4 +1,6 @@
 const User = require('../Model/user.model')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 const getUsers = async(req,res)=>{
     try{
@@ -30,14 +32,17 @@ const createUser = async(req,res)=>{
 
         const exsistingUser = await User.findOne({email});
         if(exsistingUser){
-            return res.status(400).json({message:"User with this email already exsist"})
+            return res.status(400).json({message:"User with this email already exist"})
         }
+
+        
 
         const newUser= new User({
             username,
             email,
             password
         })
+
 
         await newUser.save()
 
@@ -51,9 +56,74 @@ const createUser = async(req,res)=>{
     }
 }
 
+const updateUser = async (req, res) => {
+    const { id } = req.params;
+    const { username, email, password } = req.body;
+    
+    try {
+        const user = await User.findById(id);
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
 
 
+        const updatedUser = await User.findByIdAndUpdate(
+            id,
+            { username, email, password },
+            { new: true }
+        );
+
+        res.status(200).json({
+            success: true,
+            message: "User updated successfully",
+            user: updatedUser
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
 
 
-module.exports = {getUsers,createUser}
+const loginUser = async(req,res)=>{
+    const{username,email,password} = req.body
+
+    try{
+
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const user = await User.findOne({email})
+
+        if(!user){
+            return res.send(400).json({message:"The user was not found"})
+        }
+
+        
+
+        const token  = jwt.sign(
+            {
+            userId :user._id,
+            email : user.email,
+            username : user.username
+        },process.env.JWT_SECRET,{expiresIn:"1hr"})
+
+        res.status(200).json({
+            message:"Login Successfully",
+            token,
+            user: {
+                userId: user._id,
+                username: user.username,
+                email: user.email
+            }
+        })
+
+
+    }catch(error){
+        res.status(500).json({ error: error.message });
+    }
+}
+
+module.exports = {getUsers,createUser,updateUser,loginUser}
 
--- server/Routes/user.route.js
+++ server/Routes/user.route.js
@@ -1,8 +1,10 @@
 const express= require('express')
 const router = express.Router()
-const {getUsers,createUser} = require('../Controller/user.controller')
+const {getUsers,createUser,updateUser,loginUser} = require('../Controller/user.controller')
 
 router.get('/get',getUsers)
 router.post('/post',createUser)
+router.put('/put/:id',updateUser)
+router.post('/login',loginUser)
 
 module.exports = router
