4a2f07be09f73a9271d835c7e3f9ed93fe27c1bf
Aryaan007-sudo <blankgaming75@gmail.com>
2025-05-21 00:04:49 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const connectDB = require('./Config/db')
 const {userRouter} = require('./Routes/user.routes')
+const {gameRouter} = require('./Routes/game.routes')
 
 
 
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/user',userRouter)
-
+app.use('/games', gameRouter);
 
 
 app.get('/', (req, res) => {
