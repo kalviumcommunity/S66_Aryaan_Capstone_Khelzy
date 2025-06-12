fb627d92581953804c31a07c09c723fdb415fbe4
Aryaan007-sudo <blankgaming75@gmail.com>
2025-06-12 12:52:34 +0530

    changes

--- server/Routes/comment.routes.js
+++ server/Routes/comment.routes.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { addComment, getCommentsByGame,updateComment,deleteComment } = require('../Controller/comment.controller')
+const { addComment, getCommentsByGame } = require('../Controller/comment.controller')
 const {verifyToken}  = require('../MiddleWare/authMiddleware')
 
 router.post('/:gameId', verifyToken,addComment);        // POST a comment for a game
 router.get('/:gameId',getCommentsByGame);  // GET all comments for a game
-router.put('/:commentId',verifyToken,updateComment)
-router.delete('/:commentId',verifyToken,deleteComment)
 
 
 module.exports = router;
